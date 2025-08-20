a9bc3b4f052b1a403467faa231d50cced9d00b03
samiunmahmud23 <samiunmahmud001@gmail.com>
2025-08-20 18:39:09 +0600

    samiuns work

---
+++ Samiun/palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Iterative Fibonacci function
+int fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    int a = 0, b = 1, c;
+    for (int i = 2; i <= n; ++i) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "F(" << n << ") = " << fibonacci(n) << endl;
+    return 0;
+}
+
